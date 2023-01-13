6a02e0cb6cfad440519b4fecb2cf10e6c5cc3be8
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2023-01-13 17:45:09 +0300

    edit html

--- src/components/results/CarGrid.tsx
+++ src/components/results/CarGrid.tsx
@@ -19,213 +19,234 @@ function CarGrid({ isGridView }: { isGridView: Boolean }) {
       }}
     >
       {cars.map((car, i) => (
-        <NavLink
-          key={car.id}
-          to="description"
-          state={car}
-          style={{ width: "100%" }}
+        <Box
+        key={car.id}
+          sx={{
+            width: "100%",
+            bgcolor: '#fbfbfb',
+            ":nth-child(3n+2)": {
+              bgcolor: "#ffffff",
+            },
+          }}
         >
-          <motion.div
-            whileHover={{ scale: 1.02 }}
-            initial={{ opacity: 0, scale: 0.2 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              default: {
-                duration: 0.5,
-                ease: [0, 0.71, 0.2, 1.01],
-              },
-              scale: {
-                // type: "spring",
-                damping: 10,
-                stiffness: 90,
-                restDelta: 0.001,
-              },
-            }}
+          <NavLink
+
+            to="description"
+            state={car}
+            style={{ width: "100%" }}
           >
-            <Box
-              key={car.id}
-              sx={{
-                width: {  md: "95%" },
-                maxWidth: '100vw',
-                mx: "auto",
-                p: 0.5,
-                // py: 1,
-                ":hover": {
-                  boxShadow: 2,
-                  border: "1px solid #ff4605",
+            <motion.div
+              whileHover={{ scale: 1.02 }}
+              initial={{ opacity: 0, scale: 0.2 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                default: {
+                  duration: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01],
                 },
-                border: "1px solid #eef0f1",
-                transition: "0.7s ease-in-out",
+                scale: {
+                  // type: "spring",
+                  damping: 10,
+                  stiffness: 90,
+                  restDelta: 0.001,
+                },
               }}
             >
-              <Box>
-                <img width="100%" src={car.images[i]} alt="" />
-              </Box>
               <Box
+                key={car.id}
                 sx={{
-                  color: "#585a6a",
-                  textAlign: "left",
-                  display: "flex",
-                  gap: 3,
-                  flexDirection: "column",
+                  width: { md: "95%" },
+                  maxWidth: "100vw",
+                  mx: "auto",
+                  p: 0.5,
+                  // py: 1,
+                  ":hover": {
+                    boxShadow: 2,
+                    border: "1px solid #ff4605",
+                  },
+                  border: "1px solid #eef0f1",
+                  transition: "0.7s ease-in-out",
                 }}
               >
                 <Box>
-                  <Typography>{car.title}</Typography>
+                  <img
+                    width="100%"
+                    src={car.images[i]}
+                    alt=""
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    color: "#585a6a",
+                    textAlign: "left",
+                    display: "flex",
+                    gap: 3,
+                    flexDirection: "column",
+                  }}
+                >
                   <Box>
-                    <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
-                      <Star sx={{ fontSize: "1rem" }} />
-                    </IconButton>
-                    <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
-                      <Star sx={{ fontSize: "1rem" }} />
-                    </IconButton>
-                    <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
-                      <Star sx={{ fontSize: "1rem" }} />
-                    </IconButton>
-                    <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
-                      <Star sx={{ fontSize: "1rem" }} />
-                    </IconButton>
-                    <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
-                      <Star sx={{ fontSize: "1rem" }} />
-                    </IconButton>
+                    <Typography>{car.title}</Typography>
+                    <Box>
+                      <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
+                        <Star sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                      <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
+                        <Star sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                      <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
+                        <Star sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                      <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
+                        <Star sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                      <IconButton sx={{ color: "#ed1f1f", p: 0.5 }}>
+                        <Star sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                    </Box>
+                    <Typography variant="subtitle1">
+                      Starting from{" "}
+                      <Typography
+                        component="span"
+                        sx={{ color: "#ed1f1f" }}
+                      >
+                        Ksh {car.price}
+                      </Typography>
+                    </Typography>
                   </Box>
-                  <Typography variant="subtitle1">
-                    Starting from{" "}
-                    <Typography component="span" sx={{ color: "#ed1f1f" }}>
-                      Ksh {car.price}
-                    </Typography>
-                  </Typography>
-                </Box>
 
-                <Box>
-                  <Typography
-                    sx={{
-                      textTransform: "uppercase",
-                      color: "#585a6a",
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      fontSize: "0.7rem",
-                    }}
-                  >
-                    Engine type:{" "}
+                  <Box>
                     <Typography
-                      variant="subtitle1"
                       sx={{
-                        textTransform: "capitalize",
+                        textTransform: "uppercase",
+                        color: "#585a6a",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
                         fontSize: "0.7rem",
-                        color: "#c1c1c1",
                       }}
                     >
-                      Four Cylinder
+                      Engine type:{" "}
+                      <Typography
+                        variant="subtitle1"
+                        sx={{
+                          textTransform: "capitalize",
+                          fontSize: "0.7rem",
+                          color: "#c1c1c1",
+                        }}
+                      >
+                        Four Cylinder
+                      </Typography>
                     </Typography>
-                  </Typography>
 
-                  <Typography
-                    sx={{
-                      textTransform: "uppercase",
-                      color: "#585a6a",
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      fontSize: "0.7rem",
-                    }}
-                  >
-                    Transmission:{" "}
                     <Typography
-                      variant="subtitle1"
                       sx={{
-                        textTransform: "capitalize",
+                        textTransform: "uppercase",
+                        color: "#585a6a",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
                         fontSize: "0.7rem",
-                        color: "#c1c1c1",
                       }}
                     >
-                      {car.transmission}
+                      Transmission:{" "}
+                      <Typography
+                        variant="subtitle1"
+                        sx={{
+                          textTransform: "capitalize",
+                          fontSize: "0.7rem",
+                          color: "#c1c1c1",
+                        }}
+                      >
+                        {car.transmission}
+                      </Typography>
                     </Typography>
-                  </Typography>
 
-                  <Typography
-                    sx={{
-                      textTransform: "uppercase",
-                      color: "#585a6a",
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      fontSize: "0.7rem",
-                    }}
-                  >
-                    Fuel type:{" "}
                     <Typography
-                      variant="subtitle1"
                       sx={{
-                        textTransform: "capitalize",
+                        textTransform: "uppercase",
+                        color: "#585a6a",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
                         fontSize: "0.7rem",
-                        color: "#c1c1c1",
                       }}
                     >
-                      {car.fuelType}
+                      Fuel type:{" "}
+                      <Typography
+                        variant="subtitle1"
+                        sx={{
+                          textTransform: "capitalize",
+                          fontSize: "0.7rem",
+                          color: "#c1c1c1",
+                        }}
+                      >
+                        {car.fuelType}
+                      </Typography>
                     </Typography>
-                  </Typography>
 
-                  <Typography
-                    sx={{
-                      textTransform: "uppercase",
-                      color: "#585a6a",
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1,
-                      fontSize: "0.7rem",
-                    }}
-                  >
-                    vehicle ID:{" "}
                     <Typography
-                      variant="subtitle1"
                       sx={{
-                        textTransform: "capitalize",
+                        textTransform: "uppercase",
+                        color: "#585a6a",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
                         fontSize: "0.7rem",
-                        color: "#c1c1c1",
                       }}
                     >
-                      Qx8qx8imZ
+                      vehicle ID:{" "}
+                      <Typography
+                        variant="subtitle1"
+                        sx={{
+                          textTransform: "capitalize",
+                          fontSize: "0.7rem",
+                          color: "#c1c1c1",
+                        }}
+                      >
+                        Qx8qx8imZ
+                      </Typography>
                     </Typography>
-                  </Typography>
-                </Box>
-                <Box>
-                  <NavLink to="/description" state={car}>
-                    <Button
-                      disableElevation={true}
-                      sx={{
-                        py: 0.5,
-                        px: 0.7,
-                        borderRadius: 0,
-                        bgcolor: "#585a6a",
-                        color: "#fff",
-                        ":hover": { bgcolor: "#585a6a" },
-                      }}
+                  </Box>
+                  <Box>
+                    <NavLink
+                      to="/description"
+                      state={car}
                     >
-                      Description
-                    </Button>
-                  </NavLink>
-                  <NavLink to="/quote">
-                    <Button
-                      disableElevation={true}
-                      sx={{
-                        py: 0.5,
-                        px: 0.7,
-                        borderRadius: 0,
-                        bgcolor: "#ed1f1f",
-                        color: "#fff",
-                        ":hover": { bgcolor: "#ed1f1f" },
-                      }}
-                    >
-                      get quote
-                    </Button>
-                  </NavLink>
+                      <Button
+                        disableElevation={true}
+                        sx={{
+                          py: 0.5,
+                          px: 0.7,
+                          borderRadius: 0,
+                          bgcolor: "#585a6a",
+                          color: "#fff",
+                          ":hover": { bgcolor: "#585a6a" },
+                        }}
+                      >
+                        Description
+                      </Button>
+                    </NavLink>
+                    <NavLink to="/quote">
+                      <Button
+                        disableElevation={true}
+                        sx={{
+                          py: 0.5,
+                          px: 0.7,
+                          borderRadius: 0,
+                          bgcolor: "#ed1f1f",
+                          color: "#fff",
+                          ":hover": { bgcolor: "#ed1f1f" },
+                        }}
+                      >
+                        get quote
+                      </Button>
+                    </NavLink>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          </motion.div>
-        </NavLink>
+            </motion.div>
+          </NavLink>
+        </Box>
       ))}
     </Box>
   );
